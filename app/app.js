e9d8a9f20040f3a795d5753ed4af401fc1de183e
Mohibullah Bukhari <mohibullahbukhari98@gmail.com>
2022-02-04 01:31:48 +0500

    First Commit

---
+++ app/app.js
@@ -0,0 +1,103 @@
+const app = () => {
+  const song = document.querySelector(".song");
+  const play = document.querySelector(".play");
+  //   const replay = document.querySelector(".replay");
+  const outline = document.querySelector(".moving-outline circle");
+  const video = document.querySelector(".vid-container video");
+
+  //Sounds
+  const sounds = document.querySelectorAll(".sound-picker button");
+
+  //Time Display
+  const timeDisplay = document.querySelector(".time-display");
+  const timeSelect = document.querySelectorAll(".time-select button");
+
+  //Get the length of the outline
+  const outlineLength = outline.getTotalLength();
+  console.log(outlineLength);
+
+  //Duration
+  let fakeDuration = 600;
+
+  //   outline.style.strokeDasharray = 100;
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
+
+  //Pick different sounds
+  sounds.forEach((sound) => {
+    sound.addEventListener("click", function () {
+      song.src = this.getAttribute("data-sound");
+      video.src = this.getAttribute("data-video");
+      checkPlaying(song);
+    });
+  });
+
+  //Play Sounds
+  play.addEventListener("click", () => {
+    checkPlaying(song);
+  });
+
+  //Replay
+
+  //   replay.addEventListener("click", function () {
+  //     restartSong(song);
+  //   });
+
+  //Select Timer
+  timeSelect.forEach((option) => {
+    option.addEventListener("click", function () {
+      restartSong(song);
+      fakeDuration = this.getAttribute("data-time");
+      timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+        fakeDuration % 60
+      )}`;
+    });
+  });
+
+  //Create a function specific to stop and play the sounds
+  const checkPlaying = (song) => {
+    if (song.paused) {
+      song.play();
+      video.play();
+      play.src = "../svg/pause.svg";
+    } else {
+      song.pause();
+      video.pause();
+      play.src = "../svg/play.svg";
+    }
+  };
+
+  //   Restart Song
+  const restartSong = (song) => {
+    let currentTime = song.currentTime;
+    song.currentTime = 0;
+    console.log("Song Restarted");
+  };
+
+  //We can animate the circle
+
+  song.ontimeupdate = () => {
+    let currentTime = song.currentTime;
+    let elapsed = fakeDuration - currentTime;
+    let seconds = Math.floor(elapsed % 60);
+    let minutes = Math.floor(elapsed / 60);
+
+    //Animate the circle
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
+
+    //Animate the text
+    timeDisplay.textContent = `${minutes}:${seconds}`;
+    // timeDisplay.innerHTML = `${minutes}:${seconds}`;
+
+    if (currentTime >= fakeDuration) {
+      song.pause();
+      song.currentTime = 0;
+      //   play.src = "../svg/play.svg";
+      play.src = "../svg/play.svg";
+      video.pause();
+    }
+  };
+};
+
+app();
